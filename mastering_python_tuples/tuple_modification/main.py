22e1e0777b66cac0f7da72dba6c241386d3e0885
dsquare268 <>
2026-03-23 17:45:19 +0000

    Tuple modification

--- mastering_python_tuples/tuple_modification/main.py
+++ mastering_python_tuples/tuple_modification/main.py
@@ -1,16 +1,18 @@
 movie_genres = ("Action", "Comedy", "Drama", "Horror", "Sci-Fi")
 
-# Write your code here
+# Convert tuple to list
 temp_list = list(movie_genres)
-
+# show contents of list
 print("Prior genre:", temp_list)
 
+# change contents of list
 temp_list[2] = "Thriller"  
 temp_list[3] = "Adventure"  
 
-
+# change the list back to tuple
 movie_genres = tuple(temp_list)
 
+# clear the list
 temp_list.clear()
 
 # Testing
