45480e62ad7440e8deb47862d4490d5a4a523f86
dsquare268 <>
2026-03-23 02:16:28 +0000

    Dictionary Basics

---
+++ mastering_python_dictionaries/dictionary_basics/main.py
@@ -0,0 +1,9 @@
+# Write your code here
+library = {"title":"The Greate Gatsby",
+            "author":"F. Scott Fitzgerald",
+            "title2":"1984", "author2":"George Orwell",
+            "title3":"To Kill a Mockingbird", "author3":"Harper Lee"   
+            }
+
+# Testing
+print('Library dictionary:', library)
